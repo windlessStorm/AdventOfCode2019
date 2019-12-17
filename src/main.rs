d546a5372e7bbeb36e7a0cf10418dc59886d0224
Vikrant Biswas <vikrant.biswas@gmail.com>
2019-12-17 20:46:22 +0530

    added back day2 5 7 !

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,20 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
-// mod day5;
+mod day5;
 mod day6;
-// mod day7;
+mod day7;
 mod day8;
 mod day9;
 
 fn main() {
     day1::day1();
-    // day2::day2();
+    day2::day2();
     day3::day3();
     day4::day4();
-    // day5::day5();
+    day5::day5();
     day6::day6();
-    // day7::day7();
+    day7::day7();
     day8::day8();
     day9::day9();
 }
